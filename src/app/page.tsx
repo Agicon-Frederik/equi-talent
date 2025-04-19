cf7c7c2dd68674d1150ee4a75878eb10a59db5a0
Frederik Vantroys <frederik@agicon.be>
2025-04-19 13:34:30 +0200

    connect postgre and prisma setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,7 @@
-import { Button } from "@/components/ui/button";
-import {
-  SignedOut,
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  UserButton,
-} from "@clerk/nextjs";
-
 export default function Home() {
   return (
     <div>
-      <SignedOut>
-        <SignInButton mode="modal">
-          <Button>Log in</Button>
-        </SignInButton>
-        <SignUpButton mode="modal" />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
+      <h1>HOME page</h1>
     </div>
   );
 }
